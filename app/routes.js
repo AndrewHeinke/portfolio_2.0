f065ef11acb922ee7fc7310f276801d34741954f
Andrew <andrew.j.heinke@gmail.com>
2016-10-11 17:43:21 -0500

    updated nav styles added portfolio page route and template

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,8 @@ module.exports = function(app) {
   app.get('/', function(req, res) {
     res.render('pages/home');
   });
+  
+  app.get('/portfolio', function(req, res) {
+    res.render('pages/portfolio');
+  });
 };
