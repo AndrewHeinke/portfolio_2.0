6b936c08920f8542bd645085cbca5fc686f0c9bf
Andrew Heinke <andrew.j.heinke@gmail.com>
2016-10-12 13:57:05 -0500

    added contact page

--- app/routes.js
+++ app/routes.js
@@ -3,8 +3,16 @@ module.exports = function(app) {
   app.get('/', function(req, res) {
     res.render('pages/home');
   });
-  
+
   app.get('/portfolio', function(req, res) {
     res.render('pages/portfolio');
+  });
+
+  app.get('/resume', function(req, res) {
+    res.render('pages/resume');
+  });
+
+  app.get('/contact', function(req, res) {
+    res.render('pages/contact');
   });
 };
